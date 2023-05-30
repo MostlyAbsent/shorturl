b66f96714d7f5887c090691e2c2d15fef55f92e9
Jacob Doran <j.doran.admin@gmail.com>
2023-05-30 20:16:58 +0800

    Fix: if syntax, incorrect parens

--- src/clj/shorturl/env.clj
+++ src/clj/shorturl/env.clj
@@ -1,9 +1,9 @@
 (ns shorturl.env)
 
 (def envvar
-  (if (.exists (clojure.java.io/file "env.edn")
-               (clojure.edn/read-string (slurp "env.edn"))
-               {})))
+  (if (.exists (clojure.java.io/file "env.edn"))
+    (clojure.edn/read-string (slurp "env.edn"))
+    {}))
 
 (defn env [k]
   (or (k envvar)
